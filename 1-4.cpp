6c5b5fa6bea796c745884be6de7194d524725adb
rumidu <hyunskyu@gmail.com>
2020-06-01 02:33:22 +0900

    BAEKJOON 1-4

---
+++ 1-4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	std::cout << "|\\_/|" << std::endl;
+	std::cout << "|q p|   /}" << std::endl;
+	std::cout << "( 0 )\"\"\"\\" << std::endl;
+	std::cout << "|\"^\"`    |" << std::endl;
+	std::cout << "||_/=\\\\__|" << std::endl;
+
+}
