3365b77627960f243aab59c39834d799e25c7097
rumidu <hyunskyu@gmail.com>
2020-06-12 17:28:06 +0900

    BAEKJOON 6-4

---
+++ 6-4.cpp
@@ -0,0 +1,26 @@
+/*
+#include <iostream>
+
+int main() {
+	int count = 0;
+	int num[10] = {};
+	int arr[42] = {};
+
+	for (int i = 0; i < 10; i++) {
+		std::cin >> num[i];
+		num[i] = num[i] % 42;
+		arr[num[i]] += 1;
+	}
+
+
+	for (int j = 0; j < 42; j++) {
+
+		if (arr[j] > 0) {
+			count += 1;
+		}
+	}
+
+	std::cout << count;
+
+}
+*/
