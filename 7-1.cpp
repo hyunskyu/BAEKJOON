811418ca6852939d823b666bdc4b4f281ff4230d
rumidu <hyunskyu@gmail.com>
2020-06-18 19:27:10 +0900

    BAEKJOON 7-1

---
+++ 7-1.cpp
@@ -0,0 +1,13 @@
+/*
+#include <vector>
+
+long long sum(std::vector<int>& a) {
+	long long ans = 0;
+	int lenth = a.size();
+	for (int i = 0; i < lenth; i++) {
+		ans += a[i];
+	}
+	
+	return ans;
+}
+*/
